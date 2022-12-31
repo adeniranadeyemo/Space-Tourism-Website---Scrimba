ba45afe096617ea0c711258b3b61ef19eb23fc5b
Adeniran Adeyemo <89475955+adeniranadeyemo@users.noreply.github.com>
2022-12-31 18:45:54 +0100

    Finished up with the main pages

--- script.js
+++ script.js
@@ -14,16 +14,16 @@
 
 // OR
 
-const nav = document.querySelector('.primary-navigation')
-const navToggle = document.querySelector('.mobile-nav-toggle')
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener('click', () => {
-  const visibility = nav.getAttribute('data-visible');
-  if (visibility === 'false') {
-    nav.setAttribute('data-visible', true)
-    navToggle.setAttribute('aria-expanded', true)
+navToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   } else {
-    nav.setAttribute('data-visible', false)
-    navToggle.setAttribute('aria-expanded', false)
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
-})
+});
